0ac82719925323729f3019da2d64093a3a911554
Jianjun Lv <jianjlv@microsoft.com>
2019-09-06 15:19:49 +0800

    add package demo code

---
+++ IDEAs/PackageBuildResult.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+    Package the IDEAs project build reuslt by power shell.
+#>
+param(
+    [string]$ServiceFilesPrefix = "D:\IDEAs\Ibiza\Source\Services\DataCop\",
+    [string]$ServiceFilesSuffix = "\bin\Release\*",
+    [string]$TargetZipFilePrefix = "D:\IDEAs\Build\DeployZip\",
+    [string]$TargetZipFileSuffix = ".zip"
+)
+
+### BEGIN FUNCTIONS ####################################################
+
+function PackageAllFile ([string]$souceFile, [string]$targetZipFile) {
+    # Will throw exception when file not existing
+    # Remove-Item -Path $targetZipFile -Recurse -Force
+    Compress-Archive -Path $souceFile -DestinationPath $targetZipFile
+}
+
+function DelAllFileInOrder ([string]$targetFolder) {
+    $Files = get-childitem $targetFolder -force
+    Foreach ($File in $Files)
+    {
+        $FilePath=$File.FullName
+        Remove-Item -Path $FilePath -Recurse -Force
+    }
+}
+
+DelAllFileInOrder $TargetZipFilePrefix
+PackageAllFile $ServiceFilesPrefix"AdlsWorker"$ServiceFilesSuffix $TargetZipFilePrefix"AdlsWorker"$TargetZipFileSuffix
+PackageAllFile $ServiceFilesPrefix"CosmosWorker"$ServiceFilesSuffix $TargetZipFilePrefix"CosmosWorker"$TargetZipFileSuffix
+PackageAllFile $ServiceFilesPrefix"OndemandDataCopService"$ServiceFilesSuffix $TargetZipFilePrefix"OndemandDataCopService"$TargetZipFileSuffix
+PackageAllFile $ServiceFilesPrefix"Orchestrator"$ServiceFilesSuffix $TargetZipFilePrefix"Orchestrator"$TargetZipFileSuffix
