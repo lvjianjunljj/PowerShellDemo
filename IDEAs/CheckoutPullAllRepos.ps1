df0ebf87771f0824aeff7aa11a216b2e15e44372
Jianjun Lv <jianjlv@microsoft.com>
2020-11-24 21:16:13 +0800

    Add a new operation

---
+++ IDEAs/CheckoutPullAllRepos.ps1
@@ -0,0 +1,20 @@
+<#
+.SYNOPSIS
+    Checkout and pull code for all repos in the root folder.
+#>
+param(
+    [string]$RootFolder = "D:\IDEAs\pls_repos\"
+)
+
+### BEGIN FUNCTIONS ####################################################
+$Repos = get-childitem $RootFolder -force
+cd $RootFolder
+Foreach ($Repo in $Repos)
+{
+    $RepoPath=$Repo.FullName
+    cd $RepoPath
+    git checkout .
+    git clean -df
+    git pull
+    cd ..
+}
